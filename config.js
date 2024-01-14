7c6e035a117ca689c46ba12ba434d15f9f871cc9
ntrongphuc1302 <ntrongphuc.1302@gmail.com>
2024-01-15 00:07:30 +0700

    main

---
+++ config.js
@@ -0,0 +1,29 @@
+/*
+ * Please Read.
+ * Google  on how to get your shared_secret and identity secret. (You may have to use Steam SDA to get these codes.)
+ * gameCode is the appId of the game. You can use any appId that you have current tradable items in. Please see below examples (YOU CANNOT USE CSGO)
+ *     TF2 = 440
+ *     Witch It = 559650
+ * Use google to find more appIds.
+
+*/
+
+module.exports = {
+  account1: {
+    shared_secret: "randomStringOfChars418=",
+    identity_secret: "randomStringOfChars21w=",
+    username: "Robbie",
+    password: "Password",
+    tradelink:
+      "https://steamcommunity.com/tradeoffer/new/?partner=388964213&token=Tn8Zdodp",
+  },
+  account2: {
+    shared_secret: "randomStringOfChars418=",
+    identity_secret: "randomStringOfChars21w=",
+    username: "Robbie",
+    password: "Password",
+    tradelink:
+      "https://steamcommunity.com/tradeoffer/new/?partner=1259226280&token=35bOseRX",
+  },
+  gameCode: 673950,
+};
